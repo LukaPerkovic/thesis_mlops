60e452a0df66a187cd8d6044495bee8b00b76864
LukaPerkovic <luka.m.perkovic@gmail.com>
2025-06-14 19:10:16 +0200

    First IaC

---
+++ src/iac/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = var.aws_region
+}
+
+resource "aws_s3_bucket" "dev_bucket" {
+    bucket = var.dev_bucket_name
+
+    tags = {
+        Name        = "Dev Data Bucket"
+        Environment = "Development"
+    }
+}
+
+resource "aws_s3_bucket" "prod_bucket" {
+    bucket = var.prod_bucket_name
+
+    tags = {
+        Name        = "Prod Data Bucket"
+        Environment = "Production"
+    }
+}
---
+++ src/iac/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+    description = "The AWS region to deploy resources in"
+    type        = string
+    default     = "eu-west-3" # Paris
+}
+
+variable "dev_bucket_name" {
+    description = "The name of the S3 bucket [DEVELOPMENT]."
+    type        = string
+    default     = "dev-lp-thesis-bucket-25913513"
+}
+
+variable "prod_bucket_name" {
+    description = "The name of the S3 bucket [PRODUCTION]."
+    type        = string
+    default     = "prod-lp-thesis-bucket-25913513"
+}
