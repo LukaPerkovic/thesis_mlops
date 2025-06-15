90402cf00f0e8bc8588dd3e85503391283e43984
LukaPerkovic <luka.m.perkovic@gmail.com>
2025-06-15 14:22:44 +0200

    Enabled both dev and prod tf scripts

--- src/iac/dev/variables.tf
+++ src/iac/dev/variables.tf
@@ -4,8 +4,19 @@ variable "aws_region" {
     default     = "eu-west-3" # Paris
 }
 
-variable "dev_bucket_name" {
+variable "bucket_name" {
     description = "The name of the S3 bucket [DEVELOPMENT]."
     type        = string
     default     = "dev-lp-thesis-bucket-25913513"
+}
+
+variable "github_actions_role_name" {
+    description = "The name of the IAM role for GitHub Actions."
+    type        = string
+    default     = "GitHubActionsRoleDev"
+}
+
+variable "account_id" {
+    description = "The AWS account ID."
+    type        = string
 }
--- src/iac/prod/variables.tf
+++ src/iac/prod/variables.tf
@@ -4,8 +4,19 @@ variable "aws_region" {
     default     = "eu-west-3" # Paris
 }
 
-variable "prod_bucket_name" {
+variable "bucket_name" {
     description = "The name of the S3 bucket [PRODUCTION]."
     type        = string
     default     = "prod-lp-thesis-bucket-25913513"
+}
+
+variable "github_actions_role_name" {
+    description = "The name of the IAM role for GitHub Actions."
+    type        = string
+    default     = "GitHubActionsRoleProd"
+}
+
+variable "account_id" {
+    description = "The AWS account ID."
+    type        = string
 }
