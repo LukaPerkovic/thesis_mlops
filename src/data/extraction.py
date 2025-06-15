3f287163e6609b8c0add31c943c4219b17c0699f
LukaPerkovic <luka.m.perkovic@gmail.com>
2025-06-15 20:12:57 +0200

    Inference code completed

--- src/data/extraction.py
+++ src/data/extraction.py
@@ -18,8 +18,29 @@ def load_data(file_path: str) -> pd.DataFrame:
     return df
 
 
+def save_data(data: pd.DataFrame, file_path: str) -> None:
+    if file_path.startswith("s3://"):
+        save_data_to_s3(data, parse_s3_path(*file_path))
+    else:
+        data.to_csv(file_path)
+
+
 def load_data_from_s3(bucket_name: str, file_key: str) -> pd.DataFrame:
     s3_client = boto3.client("s3")
     response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
     data = response["Body"].read().decode("utf-8")
     return pd.read_csv(StringIO(data))
+
+
+def save_data_to_s3(df: pd.DataFrame, bucket_name: str, file_key: str) -> None:
+    try:
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer, index=False)
+        s3_client = boto3.client("s3")
+        s3_client.put_object(
+            Bucket=bucket_name, Key=file_key, Body=csv_buffer.getvalue()
+        )
+        print(f"Data saved to {bucket_name}/{file_key}")
+    except Exception as e:
+        print(f"Error saving data to S3: {e}")
+        raise e
--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,11 +1,38 @@
+import argparse
 import json
+import logging
 import os
+from typing import Any, Dict, Union
 
 import pandas as pd
 import requests
+from dotenv import load_dotenv
+
+from src.data.extraction import load_data, save_data
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def create_tf_serving_json(data):
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train and register a model.")
+    parser.add_argument(
+        "--input_data_path", type=str, required=True, help="Path to the inference data."
+    )
+
+    parser.add_argument(
+        "--output_data_path",
+        type=str,
+        required=True,
+        help="Path to export inference data.",
+    )
+
+    return parser.parse_args()
+
+
+def create_serving_json(data: Union[Dict[str, Any], pd.DataFrame]) -> Dict[str, Any]:
     return {
         "inputs": {name: data[name].tolist() for name in data.keys()}
         if isinstance(data, dict)
@@ -13,21 +40,58 @@ def create_tf_serving_json(data):
     }
 
 
-def score_model(dataset):
-    url = "https://dbc-99134396-0c42.cloud.databricks.com/serving-endpoints/test_endpoint_lp_1/invocations"
+def prepare_dataset(dataset: Union[pd.DataFrame, Dict[str, Any]]) -> Dict[str, Any]:
+    return {
+        "dataframe_split": dataset.to_dict(orient="split")
+        if isinstance(dataset, pd.DataFrame)
+        else create_serving_json(dataset)
+    }
+
+
+def send_request(data_json: str) -> Dict[str, Any]:
+    url = os.getenv("ENDPOINT_URL")
     headers = {
-        "Authorization": f"Bearer {os.environ.get('DATABRICKS_TOKEN')}",
+        "Authorization": f"Bearer {os.getenv('DATABRICKS_TOKEN')}",
         "Content-Type": "application/json",
     }
-    ds_dict = (
-        {"dataframe_split": dataset.to_dict(orient="split")}
-        if isinstance(dataset, pd.DataFrame)
-        else create_tf_serving_json(dataset)
-    )
-    data_json = json.dumps(ds_dict, allow_nan=True)
-    response = requests.request(method="POST", headers=headers, url=url, data=data_json)
+
+    response = requests.post(url, headers=headers, data=data_json)
     if response.status_code != 200:
-        raise Exception(
-            f"Request failed with status {response.status_code}, {response.text}"
+        logger.error(
+            f"Request failed with status code {response.status_code}: {response.text}"
         )
+        response.raise_for_status()
+
     return response.json()
+
+
+def score_model(dataset: Union[pd.DataFrame, Dict[str, Any]]) -> Dict[str, Any]:
+    try:
+        ds_dict = prepare_dataset(dataset)
+        data_json = json.dumps(ds_dict, allow_nan=True)
+        return send_request(data_json)
+    except requests.RequestException as e:
+        logger.error(f"Error during model scoring: {e}")
+        raise
+    except json.JSONDecodeError as e:
+        logger.error(f"Error decoding JSON response: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+        raise
+
+
+def main():
+    args = parse_args()
+    data = load_data(args.input_data_path)
+
+    predictions = pd.DataFrame(score_model(data)["predictions"])
+    predictions_df = pd.concat([data["id"], predictions], axis=1)
+    logger.info("Results generated successfully.")
+
+    save_data(predictions_df, args.output_data_path)
+    logger.info("Results exported!")
+
+
+if __name__ == "__main__":
+    main()
